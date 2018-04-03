bf4fee5646f2df40c2d381ffa35ee7e1623b5fd1
DESKTOP-M5QAN8M\maryc_1mz6n6s <marycholeg@gmail.com>
2018-04-03 11:02:37 +0300

    Change structure; add authorization

---
+++ NewAuction/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NewAuction.MvcApplication" Language="C#" %>
---
+++ NewAuction/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NewAuction.Startup))]
+namespace NewAuction
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
